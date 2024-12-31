273ab4503b85c418f61641b5f4a9ca6cdb56b843
aminrezaee <rezaeea73@gmail.com>
2024-12-31 12:48:51 +0330

    add ppo agent

--- src/entities/agents/agent.py
+++ src/entities/agents/agent.py
@@ -1,19 +1,24 @@
 from abc import ABC
+
+from tensordict import TensorDict
 class Agent(ABC):
     
     def __init__(self):
         pass
     
-    def act(self, state):
+    def act(self, state , return_log_probs:bool=False):
         pass
     
-    def train(self):
+    def train(self , memory:TensorDict):
         pass
     
     def save(self):
         pass
     
     def load(self):
+        pass
+    
+    def get_state_value(self, state) -> float:
         pass
     
     
--- src/entities/agents/ppo_agent.py
+++ src/entities/agents/ppo_agent.py
@@ -3,6 +3,8 @@ from entities.features import Run
 from models.actor import Actor
 from models.critic import Critic
 import torch
+from tensordict import TensorDict
+from utils.logger import Logger
 class PPOAgent(Agent):
     def __init__(self):
         self.actor = Actor()
@@ -10,10 +12,49 @@ class PPOAgent(Agent):
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=Run.instance().training_config.learning_rate)
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=Run.instance().training_config.learning_rate)
         
-    def act(self, state:torch.Tensor) -> torch.Tensor:
+    def act(self, state:torch.Tensor, return_log_probs:bool=False) -> torch.Tensor:
         return self.actor(state)
     
-    def train(self):
+    def train(self , memory:TensorDict):
+        batch_size = Run.instance().training_config.batch_size
+        iterations_per_episode = Run.instance().training_config.iteration_count
+        epochs = Run.instance().training_config.epochs_per_iteration
+        epoch_losses = [[],[]]
+        for _ in range(epochs):
+            iteration_losses = [[],[]]
+            for _ in range(iterations_per_episode):
+                idx = torch.randperm(len(memory))
+                shuffled_memory = memory[idx]
+                batch = shuffled_memory[:batch_size]
+                action = batch['action']
+                action_log_prob = batch['action_log_prob']
+                _ , new_action_log_probs = self.act(batch['current_state'] , return_log_probs=True)
+                new_action_log_prob = new_action_log_probs.gather(1, action)
+                # critic loss
+                current_state_value = batch['current_state_value']
+                current_state_value_target = batch['current_state_value_target']
+                critic_loss:torch.Tensor = (current_state_value - current_state_value_target).pow(2).mean()
+                self.critic_optimizer.zero_grad()
+                critic_loss.backward()
+                self.critic_optimizer.step()
+                
+                # actor loss
+                advantage = batch['advantage']
+                ratio = (new_action_log_prob - action_log_prob).exp()
+                surrogate1 = ratio * advantage
+                surrogate2 = torch.clamp(ratio, 1.0 - Run.instance().ppo_config.clip_epsilon, 1.0 + Run.instance().ppo_config.clip_epsilon) * advantage
+                actor_loss:torch.Tensor = -torch.min(surrogate1, surrogate2).mean()
+                self.actor_optimizer.zero_grad()
+                actor_loss.backward()
+                self.actor_optimizer.step()
+                
+                iteration_losses[0].append(actor_loss.item())
+                iteration_losses[1].append(critic_loss.item())
+            epoch_losses[0].append(sum(iteration_losses[0])/len(iteration_losses[0]))
+            epoch_losses[1].append(sum(iteration_losses[1])/len(iteration_losses[1]))
+        episode_actor_loss = sum(epoch_losses[0])/len(epoch_losses[0])
+        episode_critic_loss = sum(epoch_losses[1])/len(epoch_losses[1])
+        Logger.log(f"Actor Loss: {episode_actor_loss} Critic Loss: {episode_critic_loss} Epoch Loss: {episode_actor_loss + episode_critic_loss}")
         pass
     
     def save(self):
