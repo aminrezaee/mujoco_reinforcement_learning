411da2647eb136aeefcd8e2cafcef3d5ea260dd3
aminrezaee <rezaeea73@gmail.com>
2025-01-02 10:23:37 +0330

    trainable stds

--- src/models/actor.py
+++ src/models/actor.py
@@ -9,15 +9,16 @@ class Actor(nn.Module):
         config = {"final_activation":None , "activation":nn.ELU , "hidden_layer_count":1 , "shapes":[64]}
         self.networks = nn.ModuleList([create_network(config, input_shape=Run.instance().network_config.input_shape,output_shape=int(3),
                                       normalize_at_the_end=False, use_bias=True) for _ in range(sub_action_count)])
+        self.actor_logstd = nn.Parameter(torch.zeros(21))
         
     def forward(self, x , module_index):
         output_mean = self.networks[module_index](x)
         mean = nn.Tanh()(output_mean)
-        std = 0.4 * torch.ones_like(mean)
+        std = self.actor_logstd[int(module_index*3):int((module_index+1)*3)].exp()
         return mean , std
     
     def act(self, x):
         sub_action_count = Run.instance().agent_config.sub_action_count
         means = [self.networks[i](x) for i in range(sub_action_count)]
-        stds = [0.4 * torch.ones_like(means[i]) for i in range(sub_action_count)]
+        stds = [self.actor_logstd[int(i*3):int((i+1)*3)].exp() for i in range(sub_action_count)]
         return means , stds
