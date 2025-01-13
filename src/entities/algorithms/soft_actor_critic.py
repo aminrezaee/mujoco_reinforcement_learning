27a45fc1896eda22f3220f98f5213d89afb0b0df
aminrezaee <rezaeea73@gmail.com>
2025-01-13 16:29:20 +0330

    refactor codes

---
+++ src/entities/algorithms/soft_actor_critic.py
@@ -0,0 +1,19 @@
+import torch
+from .base_algorithm import Algorithm
+from entities.features import Run
+from entities.timestep import Timestep
+from tensordict import TensorDict
+import numpy as np
+from torch.nn.functional import huber_loss, mse_loss
+
+
+class SoftActorCritic(Algorithm):
+
+    def test(self, visualize: bool):
+        pass
+
+    def train(self, memory: TensorDict):
+        pass
+
+    def _iterate(self):
+        pass
