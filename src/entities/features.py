5316b815ad6d8e4ff5b35674ad1a8cb012b3d36c
aminrezaee <rezaeea73@gmail.com>
2025-01-18 15:21:33 +0330

    add sac feature

--- src/entities/features.py
+++ src/entities/features.py
@@ -82,10 +82,20 @@ class PPOConfig:
 
 
 @dataclass
+class SACConfig:
+    max_grad_norm: float  # Maximum norm for the gradients
+    gamma: float  # discount factor
+    alpha: float
+    tau: float
+    target_update_interval: int
+
+
+@dataclass
 class Run(metaclass=Singleton):
     rewards_config: RewardConfig
     training_config: TrainingConfig
     ppo_config: PPOConfig
+    sac_config: SACConfig
     environment_config: EnvironmentConfig
     agent_config: AgentConfig
     network_config: NetworkConfig
@@ -134,11 +144,13 @@ class Run(metaclass=Singleton):
             torch.nn, configurations['network_config']['activation_class'])
         network_config = configurations.pop('network_config').values()
         ppo_config = configurations.pop('ppo_config').values()
+        sac_config = configurations.pop('sac_config').values()
         environment_config = configurations.pop('environment_config').values()
         agent_config = configurations.pop('agent_config').values()
         dynamic_config = configurations.pop('dynamic_config').values()
         run = Run(RewardConfig(*rewards_config), TrainingConfig(*training_config),
-                  PPOConfig(*ppo_config), EnvironmentConfig(*environment_config),
-                  AgentConfig(*agent_config), NetworkConfig(*network_config),
-                  DynamicConfig(*dynamic_config), *configurations.values())
+                  PPOConfig(*ppo_config), SACConfig(*sac_config),
+                  EnvironmentConfig(*environment_config), AgentConfig(*agent_config),
+                  NetworkConfig(*network_config), DynamicConfig(*dynamic_config),
+                  *configurations.values())
         return run
