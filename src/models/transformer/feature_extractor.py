54205231f35d2274b56eab466d661a26f8515cfb
aminrezaee <rezaeea73@gmail.com>
2025-02-15 15:47:17 +0330

    add a feature extractor

--- src/models/transformer/feature_extractor.py
+++ src/models/transformer/feature_extractor.py
@@ -1,5 +1,9 @@
 from torch.nn import Module, Sequential, Linear, TransformerEncoderLayer, TransformerEncoder
 from entities.features import Run
+"""
+    the goal of feature extractor is to predict the next timestep observation from the current observation.
+    after training the feature extractor, we use the extractor subnetwork to extract features from the observation
+    """
 
 
 class FeatureExtractor(Module):
@@ -20,17 +24,17 @@ class FeatureExtractor(Module):
                                                 activation=self.activation_class(),
                                                 bias=self.use_bias,
                                                 batch_first=True)
-        encoder = TransformerEncoder(encoder_layer=self.encoder_layer,
+        encoder = TransformerEncoder(encoder_layer=encoder_layer,
                                      num_layers=run.network_config.num_feature_extractor_layers)
         fully_connected = Sequential(
             Linear(int(hidden_dim * run.environment_config.window_length),
-                   hidden_dim,
+                   int(hidden_dim / 2),
                    bias=self.use_bias), self.activation_class())
-        self.feature_extractor = Sequential(projection, encoder, fully_connected)
+        self.extractor = Sequential(projection, encoder, fully_connected)
         self.output_layer = Sequential(
-            Linear(hidden_dim, hidden_dim, bias=self.use_bias), self.activation_class(),
+            Linear(int(hidden_dim / 2), hidden_dim, bias=self.use_bias), self.activation_class(),
             Linear(hidden_dim, run.network_config.input_shape, bias=self.use_bias))
 
     def forward(self, x):
-        x = self.feature_extractor(x)
+        x = self.extractor(x)
         return self.output_layer(x)
